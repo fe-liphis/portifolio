7ace0b7ca2a17a47b4544dc700bc1602a6ce99c7
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-05 00:36:18 -0300

    TEST: Testando workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Felipe Gabriel - Portifólio</h1>;
+  return (
+    <>
+      <h1>Felipe Gabriel - Portifólio</h1>
+      <h2>Teste de Workflow</h2>
+    </>
+  );
 }
 
 export default App;
