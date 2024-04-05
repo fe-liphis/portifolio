d49cef46f612dd25bc0734b0cb3ef21e297185f5
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-04 23:50:17 -0300

    FEAT: Adicionado mais estilos ao theme do styled components

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,50 @@
-const theme = {};
+const theme = {
+  colors: {
+    background: {
+      primary: "#383838",
+      secondary: "#FFFFFF",
+      tertiary: "#000000",
+      quaternary: "#E7E7E7",
+      quinary: "#57CBF7",
+    },
+    text: {
+      principal: "#FFFFFF",
+      secondary: "#000000",
+      aqua: "#57F7F2",
+      blue: "#3A7DFF",
+      navyBlue: "#0500FF",
+      yellow: "#EBFF00",
+      orange: "#FF9900",
+      purple: "#EB00FF",
+    },
+  },
+  sizes: {
+    text: {
+      title: {
+        lg: "80px",
+        m: "64px",
+        x: "48px",
+        xx: "36px",
+      },
+      subtitle: {
+        lg: "36px",
+        m: "30px",
+        x: "24px",
+        xx: "20px",
+      },
+      default: {
+        lg: "36px",
+        m: "24px",
+        x: "20px",
+        xx: "18px",
+      },
+      label: {
+        lg: "24px",
+        m: "20px",
+        x: "18px",
+      },
+    },
+  },
+};
 
 export default theme;
