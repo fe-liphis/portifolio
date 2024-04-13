91e4a295b886799616c2b6823a210f0b10df9dde
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-13 11:46:35 -0300

    FEAT: Ajustes na responsividade do component <Projetos/>

--- src/components/Projetos/styles.ts
+++ src/components/Projetos/styles.ts
@@ -22,8 +22,13 @@ export const ProjetosTitulo = styled.h3`
     text-align: center;
   }
 
+  @media (max-width: 568px) {
+    margin-bottom: 35px;
+  }
+
   @media (max-width: 425px) {
     font-size: ${({ theme }) => theme.sizes.text.title.xx};
+    margin-bottom: 20px;
   }
 `;
 
@@ -31,6 +36,25 @@ export const ProjetoList = styled.ul`
   display: grid;
   grid-template-columns: repeat(3, min-content);
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    gap: 35px;
+    grid-template-columns: repeat(2, min-content);
+  }
+
+  @media (max-width: 568px) {
+    gap: 25px;
+  }
+
+  @media (max-width: 468px) {
+    gap: 15px;
+  }
+
+  @media (max-width: 425px) {
+    grid-template-columns: min-content;
+    gap: 20px;
+  }
 `;
 
 export const ProjetoItem = styled.li`
@@ -49,6 +73,30 @@ export const ProjetoItem = styled.li`
 
 export const ProjetoImg = styled.img`
   height: 200px;
+
+  @media (max-width: 1600px) {
+    height: 175px;
+  }
+
+  @media (max-width: 1400px) {
+    height: 150px;
+  }
+
+  @media (max-width: 1100px) {
+    height: 125px;
+  }
+
+  @media (max-width: 960px) {
+    height: 110px;
+  }
+
+  @media (max-width: 568px) {
+    height: 100px;
+  }
+
+  @media (max-width: 468px) {
+    height: 95px;
+  }
 `;
 
 export const ProjetoLink = styled.a`
@@ -57,11 +105,11 @@ export const ProjetoLink = styled.a`
   font-weight: bold;
   text-decoration: none;
 
-  @media (max-width: 1000px) {
+  @media (max-width: 1400px) {
     font-size: ${({ theme }) => theme.sizes.text.default.x};
   }
 
-  @media (max-width: 425px) {
+  @media (max-width: 1000px) {
     font-size: ${({ theme }) => theme.sizes.text.default.xx};
   }
 `;
