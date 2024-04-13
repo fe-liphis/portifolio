988619dd0c9f0396bb83fddd369c5d5f151482bc
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-13 12:35:43 -0300

    FEAT: Criado o component <Formacao/>

---
+++ src/components/Formacao/Formacao.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Section } from "../UI/Section";
+
+function Formacao() {
+  return (
+    <Section $bgColor="primary">
+      <h2>Formação</h2>
+    </Section>
+  );
+}
+
+export default Formacao;
