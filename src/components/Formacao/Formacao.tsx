2d8ccdd6863b188128908454abf8d81bb566be8a
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-15 18:38:59 -0300

    FEAT: Estilização inicial do component <Formacao/> finalizado

--- src/components/Formacao/Formacao.tsx
+++ src/components/Formacao/Formacao.tsx
@@ -1,47 +1,43 @@
 import Xarrow from "react-xarrows";
 import { Section } from "../UI/Section";
-import { FormacaoTitulo } from "./styles";
+import {
+  FormacaoTitulo,
+  TimelineArrowContainer,
+  TimelineDescriptionContainer,
+  TimelineText,
+  TimelineWrapper,
+} from "./styles";
 
 function Formacao() {
   return (
     <Section $bgColor="primary">
       <FormacaoTitulo>Formação</FormacaoTitulo>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateRows: "1fr 1fr 1fr",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            width: "60%",
-            marginLeft: "auto",
-            marginRight: "50px",
-          }}
-        >
-          <p id="graduacao_date_start"> 07/2023 </p>
-          <p id="graduacao_date_end"> 12/2025 </p>
+      <TimelineWrapper>
+        <TimelineDescriptionContainer>
+          <TimelineText id="graduacao_date_start" $bold $paddingLeftAndRight>
+            07/2023{" "}
+          </TimelineText>
+          <TimelineText id="graduacao_date_end" $bold $paddingLeftAndRight>
+            {" "}
+            12/2025{" "}
+          </TimelineText>
           <Xarrow
             start={"graduacao_date_start"}
             end={"graduacao_date_end"}
             dashness={true}
             showHead={false}
-            color="#57F7F2"
+            labels={
+              <TimelineText $color="aqua" $paddingBottom>
+                2° Semestre
+              </TimelineText>
+            }
+            color="white"
             showTail={false}
           />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
-          }}
-        >
-          <div id="timeline_start"></div>
-          <div id="timeline_end"></div>
+        </TimelineDescriptionContainer>
+        <TimelineArrowContainer>
+          <span id="timeline_start"></span>
+          <span id="timeline_end"></span>
           <Xarrow
             start={"timeline_start"}
             end={"timeline_end"}
@@ -52,23 +48,14 @@ function Formacao() {
             tailColor={"white"}
             showTail={true}
           />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "60%",
-            marginLeft: "auto",
-            alignItems: "center",
-            marginRight: "50px",
-          }}
-        >
-          <p id="graduacao_name">
+        </TimelineArrowContainer>
+        <TimelineDescriptionContainer>
+          <TimelineText $color="aqua" id="graduacao_name">
             Análise e desenvolvimento de sistemas <br /> Unicesumar
-          </p>
-          <p>Conclusão prevista</p>
-        </div>
-      </div>
+          </TimelineText>
+          <TimelineText>Conclusão prevista</TimelineText>
+        </TimelineDescriptionContainer>
+      </TimelineWrapper>
     </Section>
   );
 }
--- src/components/Formacao/styles.tsx
+++ src/components/Formacao/styles.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import theme from "../../styles/theme";
 
 export const FormacaoTitulo = styled.h2`
   font-size: ${({ theme }) => theme.sizes.text.title.lg};
   font-weight: bold;
   color: ${({ theme }) => theme.colors.text.principal};
-  margin-bottom: 50px;
+  margin-bottom: 75px;
 
   @media (max-width: 1600px) {
     font-size: 70px;
@@ -23,11 +24,50 @@ export const FormacaoTitulo = styled.h2`
   }
 
   @media (max-width: 568px) {
-    margin-bottom: 35px;
+    margin-bottom: 50px;
   }
 
   @media (max-width: 425px) {
     font-size: ${({ theme }) => theme.sizes.text.title.xx};
     margin-bottom: 20px;
   }
+`;
+
+export const TimelineWrapper = styled.section`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  width: 80%;
+  margin: auto;
+`;
+
+export const TimelineArrowContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const TimelineDescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+`;
+
+export const TimelineText = styled.p.attrs<{
+  $color?: keyof typeof theme.colors.text;
+  $bold?: boolean;
+  $paddingLeftAndRight?: boolean;
+  $paddingBottom?: boolean;
+}>(({ $color = "principal", $bold, $paddingLeftAndRight, $paddingBottom }) => ({
+  $color: $color,
+  $bold: $bold,
+  $paddingLeftAndRight: $paddingLeftAndRight,
+  $paddingBottom: $paddingBottom,
+}))`
+  font-weight: ${({ $bold }) => ($bold ? "bold" : "normal")};
+  color: ${({ $color }) => $color && theme.colors.text[$color]};
+  font-size: ${({ theme }) => theme.sizes.text.default.x};
+  text-align: center;
+  padding: ${({ $paddingLeftAndRight }) => $paddingLeftAndRight && "0 10px"};
+  padding-bottom: ${({ $paddingBottom }) => $paddingBottom && "50px"};
 `;
