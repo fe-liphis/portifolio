540426fd9dd36bbb4e1d630ea9fac3bf5fc59cb5
Felipe Gabriel <107822310+fe-liphis@users.noreply.github.com>
2024-04-16 15:17:40 -0300

    FEAT: Titulo do componente <Experiencias/> finalizada

--- src/components/Experiencias/Experiencias.tsx
+++ src/components/Experiencias/Experiencias.tsx
@@ -1,7 +1,12 @@
 import { Section } from "../UI/Section";
+import { ExperienciasTitulo } from "./styles";
 
 function Experiencias() {
-  return <Section $bgColor="secondary">Experiencias</Section>;
+  return (
+    <Section $bgColor="secondary">
+      <ExperienciasTitulo>Experiências</ExperienciasTitulo>
+    </Section>
+  );
 }
 
 export default Experiencias;
---
+++ src/components/Experiencias/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ExperienciasTitulo = styled.h3`
+  font-size: ${({ theme }) => theme.sizes.text.title.lg};
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.text.secondary};
+  text-align: end;
+  margin-bottom: 50px;
+
+  @media (max-width: 1600px) {
+    font-size: 70px;
+  }
+
+  @media (max-width: 1400px) {
+    font-size: ${({ theme }) => theme.sizes.text.title.m};
+  }
+
+  @media (max-width: 1000px) {
+    font-size: ${({ theme }) => theme.sizes.text.title.x};
+  }
+
+  @media (max-width: 768px) {
+    text-align: center;
+    margin-bottom: 35px;
+  }
+
+  @media (max-width: 425px) {
+    font-size: ${({ theme }) => theme.sizes.text.title.xx};
+    margin-bottom: 20px;
+  }
+`;
